d7750f5ba588e28ee0ea9d1b92ed2d763964c6a9
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-09-14 12:44:41 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,10 @@
-sudo apt-get install gcc g++ lolcat
+sudo apt update
+sudo apt-get install gcc g++ openjdk-11-jdk-headless
+mkdir ~/bin
+mv run.py ~/bin/
+export PATH="$PATH:$HOME/bin"
+echo 'export PATH="$PATH:$HOME/bin"' >> ~/.bashrc
+chmod +x ~/bin/run.py
+source ~/.bashrc
+cd
+echo 'alias runf="time python3 ~/bin/run.py -f"' >> .bashrc
